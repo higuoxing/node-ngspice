589f11b28521e255cb220048e120e341fe2817bc
higuoxing@gmail.com <higuoxing@outlook.com>
2018-04-30 10:30:39 +0800

    fix: server deploy

--- configs/default.js
+++ configs/default.js
@@ -2,5 +2,6 @@ module.exports = {
   // default configuration
   deploy_domain: 'http://localhost',      // deploy index
   port: 3000,                             // given port
+  socket_url: 'http://localhost:3000',
   tmp_file_path: '/tmp/',                  // tmp file path
 }
