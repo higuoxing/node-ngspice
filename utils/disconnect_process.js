ad27b555653607ab3b13a5de98dc436f35c5d13d
higuoxing@gmail.com <higuoxing@outlook.com>
2018-04-30 11:07:08 +0800

    update: multiuser

--- utils/disconnect_process.js
+++ utils/disconnect_process.js
@@ -1,5 +1,15 @@
 module.exports = {
   disconnect_process: async (socket) => {
     // delete all files of this id
+    const fs = require('fs');
+    const tmp_file_path = require('../configs/default').tmp_file_path;
+    await fs.unlink(tmp_file_path + socket.id + '-test.sp', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    await fs.unlink(tmp_file_path + socket.id + '-test.data', (err) => {
+      console.log(err);
+    });
   }
 }
