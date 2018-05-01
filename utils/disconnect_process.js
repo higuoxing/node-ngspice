3e37dc1580b70aca42ab741f85d0d31baa7ef8d0
higuoxing@gmail.com <higuoxing@outlook.com>
2018-05-01 20:58:16 +0800

    fix: delete error

--- utils/disconnect_process.js
+++ utils/disconnect_process.js
@@ -3,13 +3,20 @@ module.exports = {
     // delete all files of this id
     const fs = require('fs');
     const tmp_file_path = require('../configs/default').tmp_file_path;
-    await fs.unlink(tmp_file_path + socket.id + '-test.sp', (err) => {
-      if (err) {
+    try {
+      await fs.unlink(tmp_file_path + socket.id + '-test.sp', (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      await fs.unlink(tmp_file_path + socket.id + '-test.data', (err) => {
         console.log(err);
-      }
-    });
-    await fs.unlink(tmp_file_path + socket.id + '-test.data', (err) => {
-      console.log(err);
-    });
+      });
+    } catch (e) {
+      // do nothing
+      console.log(e);
+
+    }
+
   }
 }
