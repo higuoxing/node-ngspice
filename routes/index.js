6dede375077020f4b1281a8ef8664e782a32daa6
higuoxing@gmail.com <higuoxing@outlook.com>
2018-05-02 16:54:35 +0800

    fix: socket url

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const router = express.Router();
 /* GET home page. */
 router.get('/',  async (req, res, next) => {
   // passing socket config to template file
-  const deploy_domain = require('../configs/default').deploy_domain;
-  const port = require('../configs/default').port;
-  return res.render('index', { socket_conf: deploy_domain + ':' + port });
+  const socket_url = require('../configs/default').socket_url;
+  return res.render('index', { socket_conf: socket_url });
 });
 
 module.exports = router;
