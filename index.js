9b17ba237b361bc3615d9aa8d5f484bbd5626d9b
higuoxing@gmail.com <higuoxing@outlook.com>
2018-05-30 19:48:21 +0800

    fix: code style

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ const ngspice = require('./node-ngspice/wrapper/wrapper');
 
 let ng_inst = new ngspice.NgSpice(0, true);
 
-console.error(ng_inst);
-
-// libngspice.ngSpice_Init(sendChar, sendStat, controlledExit, sendData, sendInitData, BGThreadRunning, null);
-// libngspice.ngSpice_Command('source examples/4-bit-all-nand-gate-binary-adder.cir');
-// libngspice.ngSpice_Command('run');
-// console.log(libngspice.ngSpice_CurPlot());
-//
 // let ng_inst = new NgSpice();
